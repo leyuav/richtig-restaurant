a5e7849d1f190f59bafeccd34375186d3eb9aa4a
leyuav <leyu.av@gmail.com>
2024-06-16 17:01:35 +0200

    Start today's special section
    - import of wine names

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-
-import './SpecialMenu.css';
+import React from "react";
+import { images, data } from "../../constants";
+import "./SpecialMenu.css";
+import { MenuItem, SubHeading } from "../../components";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
