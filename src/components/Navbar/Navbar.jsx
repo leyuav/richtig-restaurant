90d564382e6e4c161efc1df069e70477c8f20b45
leyuav <leyu.av@gmail.com>
2024-06-16 12:08:39 +0200

    Changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import "./Navbar.css";
 const Navbar = () => (
   <nav className="app__navbar">
     <div className="app__navbar-logo">
-      <img src={images.gericht} alt="restaurant logo" />
+      <img src={images.richtig} alt="restaurant logo" />
     </div>
     <ul className="app__navbar-links">
       <li className="p__opensans">
